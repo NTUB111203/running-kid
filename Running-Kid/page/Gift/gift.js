7c5a6d586972a4116cfa5e685f50cffa1d927784
Darren <linz77411@gmailc.om>
2022-05-17 23:39:33 +0800

    新增兌換禮物頁面

---
+++ Running-Kid/page/Gift/gift.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet,Image,ScrollView,ImageBackground,View,Text,SafeAreaView} from "react-native";
+import Header from "../header";
+
+export default function Gift() {
+    return (
+        <SafeAreaView >
+      
+            <ScrollView
+              showsVerticalScrollIndicator={false}
+              bounces={false}
+              stickyHeaderIndices={[0]}>
+             <Header/>
+
+             <ImageBackground style={styles.backgroundimg} 
+            source={require('../../assets/GIFT.png')}
+            >
+
+            
+            </ImageBackground>
+            </ScrollView>
+        </SafeAreaView> 
+    )
+}
+
+const styles=StyleSheet.create({
+    
+
+    backgroundimg:{
+        width:"100%",
+        height:1400,
+        flex:1,
+     
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    
+        
+      },
+})
