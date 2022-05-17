ae276a0910f3f0e12dac63c586bec7609f88368a
Darren <linz77411@gmailc.om>
2022-05-17 23:40:14 +0800

    新增每日任務頁面

--- Running-Kid/page/mission/mission.js
+++ Running-Kid/page/mission/mission.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet,Image,ScrollView,ImageBackground,View,Text,SafeAreaView} from "react-native";
 import Header from "../header";
-import Textbox_title from "./Components/TextBox";
+import {Textbox_title,Mission_farm,Mission_farm2} from "./Components/TextBox";
 
 export default function Mission() {
     return (
@@ -16,16 +16,12 @@ export default function Mission() {
              <ImageBackground style={styles.backgroundimg} 
             source={require('../../assets/background.png')}
             >
-                
-            <Textbox_title style={{flex:1}}>
-                <Text>sada</Text>
-            </Textbox_title >
-            <Textbox_title style={{flex:1}}>
-                <Text>sada</Text>
-            </Textbox_title>
-            <Textbox_title>
-                <Text>sada</Text>
-            </Textbox_title>
+            
+            <Textbox_title/>
+            <Mission_farm/>    
+            <Mission_farm2/>    
+
+           
 
             
             </ImageBackground>
@@ -35,16 +31,15 @@ export default function Mission() {
 }
 
 const styles=StyleSheet.create({
+    
+
     backgroundimg:{
         width:'100%',
         height:950,
         flex:1,
-        left:0,
-        right:0,
-        top:0,
-        bottom:0,
         justifyContent: 'flex-start',
         alignItems: 'center',
+    
         
       },
 })
