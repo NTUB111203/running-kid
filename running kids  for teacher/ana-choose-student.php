64479fce30edb469b26d63da31f62d71491ea660
KCY <11036005@ntub.edu.tw>
2022-05-22 16:14:53 +0800

    update chart

--- running kids  for teacher/ana-choose-student.php
+++ running kids  for teacher/ana-choose-student.php
@@ -334,14 +334,14 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">班級分析</h1>
+                        <h1 class="h3 mb-0 text-gray-800">學生分析</h1>
                         <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
                     </div>
 
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">請選擇班級</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">請選擇學生</h6>
                         </div>
                     </div>
                     <!-- Content Row -->
