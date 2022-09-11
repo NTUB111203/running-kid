0f1d5409add5eff46b51ac00b4fc204aaa95581c
Chia Yin <KCY90413@gmail.com>
2022-09-11 14:21:32 +0800

    模組化（side+top)
    debug sidebar 收入按鈕修復
    new     top 教師姓名連結註冊
    -
    copy_model.php
    模組化方式
    包含（side.php+top.php)
    可複製copy_model.php 修改其他頁面

---
+++ running kids  for teacher/side.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../Logo/logo-pink.png" type="image/x-icon">
+    <title>running kids for teacher</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+</head>
+
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <img src="../Logo/logo-white.png" width="50px">
+                    <!-- <i class="fas fa-laugh-wink"></i> -->
+                </div>
+                <div class="sidebar-brand-text mx-3">孩是要運動</div>
+            </a>
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-home"></i>
+                    <span>首頁</span></a>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#exerciseANA"
+                    aria-expanded="true" aria-controls="exerciseANA">
+                    <i class="fas fa-fw  fa-chart-line"></i>
+                    <span>運動分析</span>
+                </a>
+                <div id="exerciseANA" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">運動分析項目:</h6>
+                        <a class="collapse-item" href="ana-school.php">全校分析</a>
+                        <a class="collapse-item" href="ana-choose-class.php">班級分析</a>
+                        <a class="collapse-item" href="ana-choose-student.php">學生個人分析</a>
+                    </div>
+                </div>
+            </li>
+             <!-- Nav Item - Pages Collapse Menu -->
+             <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>學校管理</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">管理項目:</h6>
+                        <a class="collapse-item" href="class-maintain.php">班級管理</a>
+                        <a class="collapse-item" href="student-maintain.php">學生管理</a>
+                        <!-- 學生運動資訊透過學生管理介面連結到 -->
+                    </div>
+                </div>
+            </li>
+
+            <!-- Nav Item - Utilities Collapse Menu -->
+            <li class="nav-item">
+               <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-gift"></i>
+                    <span>禮物管理</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">禮物兌換項目:</h6>
+                        <!-- <a class="collapse-item" href="gift-on.php">商品上架</a> -->
+                        <a class="collapse-item" href="gift.php">禮品編輯</a>
+                        <!-- <a class="collapse-item" href="gift-update.php">商品修改</a> -->
+                        <a class="collapse-item" href="gift-off.php">禮品兌換</a>
+                        <!-- <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                        <a class="collapse-item" href="utilities-other.html">Other</a> -->
+                    </div>
+                </div>
+            </li> 
+
+          
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+
+</html>
