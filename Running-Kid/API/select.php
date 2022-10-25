127183118d5e73aaa28bc7013abce96679215121
Emily Cheng <emily.sep24@gmail.com>
2022-10-25 16:27:22 +0800

    Update select.php

--- Running-Kid/API/select.php
+++ Running-Kid/API/select.php
@@ -1,18 +1,28 @@
 <?php
-    session_start();
-    
-    $mysql_host="140.131.114.154";
-    $mysql_user="emily";
-    $mysql_pass="@Tfboys806@";
-    $mysql_DB="runningkids";
-    $DB=mysqli_connect($mysql_host,$mysql_user,$mysql_pass);
+    $CN = mysqli_connect('140.131.114.154', 'emily', '@Tfboys806@', 'runningkids');
+    $DB = mysqli_select_db($CN, 'runningkids');
 
- 
-    if($DB){
-        echo "已確定連線";
-    }else{
-        echo '無法連線Mysql資料庫：<br/>'.mysqli_connect_error();
-    }
+    $EncodedData = file_get_contents('php://input');
+    $DecodedData = json_decode($EncodedData, true);
 
+   // $m_id = '194';
+    //$sch_no = 1;
+    //$m_name = 'dddfgfdf';
+    //$class_no = 1;
+    $m_id = $DecodedData['m_id'];
+    $m_name = $DecodedData['m_name'];
+    $sch_no = $DecodedData['sch_no'];
+    $class_no = $DecodedData['class_no'];
 
+    $insertMemberData = "insert into members(m_id,sch_no,m_name,class_no) values ('$m_id','$sch_no','$m_name','$class_no')";
+
+    $register = mysqli_query($CN, $insertMemberData);
+
+    if ($register) 
+        $Message = "Member has been registered successfully";
+    else 
+        $Message = "Server Error... please try latter";
+
+    $Response[] = array("Message" => $Message);
+    echo json_encode($Response);
 ?>
