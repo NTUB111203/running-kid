58419766ac3158c3fce2504b7d01f7bbb12b4f4d
AMDonnie <linz77411@gmail.com>
2022-11-01 10:44:25 +0800

    security update

--- Running-Kid/ios/RunningKid/AppDelegate.m
+++ Running-Kid/ios/RunningKid/AppDelegate.m
@@ -31,6 +31,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
