345df96ad35d77cd4ceabddbd59da51bf726d639
shihmimi <97840868+shihmimi@users.noreply.github.com>
2022-12-14 15:13:15 +0800

    register

--- running kids for teacher-test/htdocs/register.php
+++ running kids for teacher-test/htdocs/register.php
@@ -4,19 +4,22 @@ include_once("DataBase.php");
 $sch_no = $_POST['school'];
 $m_name = $_POST['name'];
 $gender = isset($_POST['gender']) ? $_POST['gender'] : 'F';
-$gender = $_POST['gender'];
+// $gender = $_POST['gender'];
 $identity = isset($_POST['identity']) ? $_POST['identity'] : 'T';
 // $identity = $_POST['identity'];
-$phone = $_POST['phone'];
+//$phone = $_POST['phone'];
 $mail = $_POST['mail'];
-$birthday = $_POST['bir'];
+//$birthday = $_POST['bir'];
 $m_id = $_POST['id'];
 $password = $_POST['password'];
 //$class = $_POST["class"];
+//$class_no = $_POST["class"];
 
+$sql = "INSERT INTO members(sch_no,identity,m_name,mail,m_id,password,class_no) 
+        VALUES ('$sch_no','$identity','$m_name','$mail','$m_id','$password','$class_no')";
 
-$sql = "INSERT INTO members(sch_no,identity,m_name,gender,phone,mail,birthday,m_id,password,class_no) 
-        VALUES ('$sch_no','$identity','$m_name','$gender','$phone','$mail','$birthday','$m_id','$password','')";
+echo $sql;
+
 if (mysqli_query($link, $sql)) {
     echo "註冊成功!!";
 } else {
