6a3ea4b76a6b6fac7c6da58a163d88bc699ab0c7
shihmimi <97840868+shihmimi@users.noreply.github.com>
2022-12-15 19:25:00 +0800

    12/15改

--- running kids for teacher-test/htdocs/gift-add.php
+++ running kids for teacher-test/htdocs/gift-add.php
@@ -1,23 +1,28 @@
 <?php
 require_once("DataBase.php");
+session_start();
+$m_name = $_SESSION["m_name"];
+$sch_no = $_SESSION["sch_no"];
+$class_no = $_SESSION["class_no"];
+
 $gift = $_POST['gift']; //禮物名稱
 $exchange_points = intval($_POST['exchange_points']); //禮物積分
 $gift_description = $_POST['gift_description']; //禮物描述
 $sup_name = $_POST['sup_name'];  //禮品供應商
 $sup_no = $_POST['sup_no'];
 //$sup_tel = $_POST['sup_tel'];  //供應商電話
- $sql = "SELECT sup_no from  gift_supplier where sup_name='$sup_name'";
+$sql = "SELECT sup_no from  gift_supplier where sup_name='$sup_name'";
 
 //     $sql = "INSERT INTO gift_supplier(sup_name) VALUES ('$sup_name')";
 //     mysqli_query($link,$sql);
 //     $sql = "Select sup_no from  gift_supplier where sup_name='$sup_name'";
 //     $check_sup = mysqli_query($link,$sql);
 //     $sup_no = mysqli_fetch_assoc($check_sup)["sup_no"];
-    // echo $sup_no;
-$sql = "INSERT INTO gift(gift,exchange_points,gift_sup_no,gift_description) VALUES ('$gift',$exchange_points,$sup_no,'$gift_description')";
+// echo $sup_no;
+$sql = "INSERT INTO gift(gift,exchange_points,gift_sup_no,gift_description,sch_no) VALUES ('$gift',$exchange_points,$sup_no,'$gift_description','$sch_no')";
 
 
 //新增成功
-if(mysqli_query($link,$sql)){
+if (mysqli_query($link, $sql)) {
     header("Location: gift.php");
 }
